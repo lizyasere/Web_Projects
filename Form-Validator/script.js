4f0460ea247f186a89cb2eb3508bbca656ba4f41
lizyasere <iyasere.elizabeth1@gmail.com>
2023-08-17 04:35:46 +0100

    Form Validator

---
+++ Form-Validator/script.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+//Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    form.control
+}
+//Add event listeners
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    if (username.value === '') {
+        showError(username, 'username is required');
+    } else{
+        showSuccess(username);
+    }
+   // console.log(username.value);
+});
